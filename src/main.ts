1f23908c6697437bf33bbaba056dbbd89321e25c
drichard1989 <drichard1989@icloud.com>
2023-03-23 14:16:30 -0500

    2 files modified, 2 added

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,17 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-    <div>{{myPublicName}}
-    test 
-  `,
+  providers: [DataService],
+  templateUrl: 'main.html',
 })
 export class App {
+  constructor(public dataService: DataService) {}
   name = 'Angular';
   private myName: string = 'Dylan';
   public myPublicName: string = 'Dylan Public';
