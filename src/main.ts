6f710ddd0956defa5f74a9c1f41af5a158d786c7
drichard1989 <drichard1989@icloud.com>
2023-03-23 14:23:08 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import {
+  OnInit,
+  AfterViewInit,
+  AfterViewChecked,
+  Component,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { DataService } from './data.service';
@@ -11,11 +16,25 @@ import { DataService } from './data.service';
   providers: [DataService],
   templateUrl: 'main.html',
 })
-export class App {
-  constructor(public dataService: DataService) {}
+export class App implements OnInit, AfterViewInit, AfterViewChecked {
+  constructor(public dataService: DataService) {
+    console.log('constructor implemented');
+  }
   name = 'Angular';
   private myName: string = 'Dylan';
   public myPublicName: string = 'Dylan Public';
+
+  ngOnInit() {
+    console.log('ngOnInit implemented');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit implemented');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked implemented');
+  }
 }
 
 bootstrapApplication(App);
