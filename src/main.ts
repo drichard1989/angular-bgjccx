f730c4661f7cd7ef48c48a5e5783d91d3385f24b
kmaddox2122 <111718502+kmaddox2122@users.noreply.github.com>
2023-03-28 22:14:56 -0500

    Added utilityService and completed variable exercise

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,22 @@ import {
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { DataService } from './data.service';
+import { UtilityService } from './utility.service';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  providers: [DataService],
+  providers: [DataService, UtilityService],
   templateUrl: 'main.html',
 })
 export class App implements OnInit, AfterViewInit, AfterViewChecked {
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, public utilityService: UtilityService) {
     console.log('constructor implemented');
+    //public variable used
+    console.log(this.utilityService.Chloe)
+    //private variable used  -- received error that "property does not exist on type UtilityService"
+    // console.log(this.utilityService.trayden);
   }
   nameTest = 'Katie'
   name = 'Angular';
---
+++ src/utility.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UtilityService {
+  public Chloe = "Chloe loves her food!";
+  private Trayden = "Trayden loves his food too!"
+
+  constructor() { }
+
+}
